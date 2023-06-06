3e1cfc9b27b8bf8c2080a7d28c90f8834eaf4c9c
Felix Anneblad <felix.anneblad@gmail.com>
2023-06-06 13:28:34 +0200

    my-npm-axios-dog package working

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ highestInput.setAttribute("type", "number");
 const noPara = document.createElement("div")
 
 const rdmBtn = document.createElement("button");
-rdmBtn.textContent = "Random no"
+rdmBtn.textContent = "Random NO"
 rdmBtn.addEventListener("click", ()=>{
     const lowest = parseInt(lowestInput.value);
     const highest = parseInt(highestInput.value);
@@ -24,3 +24,28 @@ fieldDiv.appendChild(highestInput)
 fieldDiv.appendChild(rdmBtn)
 fieldDiv.appendChild(noPara)
 
+const { getRandomDogImage } = require('my-npm-axios-dog');
+const outDogImg = document.createElement('div')
+
+async function dogFun() {
+    try {
+      const dogImgUrl = await getRandomDogImage();
+      const dogImg = document.createElement("img");
+      outDogImg.innerHTML = '';
+      dogImg.src = dogImgUrl;      
+      dogImg.style.maxWidth = "300px"; // Set the maximum width
+      dogImg.style.maxHeight = "300px"; // Set the maximum height
+      outDogImg.appendChild(dogImg); // Append the image to the dogDiv element
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  }
+  
+  const dogDiv = document.getElementById("rdm-dog-field");
+  const rdmDogBtn = document.createElement("button");
+  rdmDogBtn.textContent = "Random dog";
+  rdmDogBtn.addEventListener("click", dogFun); // Use dogFun as the event listener
+  
+  dogDiv.appendChild(outDogImg)
+  dogDiv.appendChild(rdmDogBtn);
+  
